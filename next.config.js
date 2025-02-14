10a6971d1393dc1ec956207ac3a936d401c79e87
Peerapat Toprasert <peerapat.toprasert@gmail.com>
2025-02-14 13:12:44 +0700

    fix:remove rewrites

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@
 const nextConfig = {
   // reactStrictMode: true,
   // swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://sea-lion-app-yh6w6.ondigitalocean.app/api/:path*', // Ensure this is correctly set
-      },
-    ]
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'https://sea-lion-app-yh6w6.ondigitalocean.app/api/:path*', // Ensure this is correctly set
+  //     },
+  //   ]
+  // },
 }
 
 module.exports = nextConfig
