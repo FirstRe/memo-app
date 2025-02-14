5be8ba7515c914f7a73fd6cf67fd97e86f58a822
Peerapat Toprasert <peerapat.toprasert@gmail.com>
2025-02-14 12:58:54 +0700

    fix next config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ const nextConfig = {
       {
         source: '/api/:path*',
         destination: 'https://sea-lion-app-yh6w6.ondigitalocean.app/api/:path*', // Ensure this is correctly set
-        permanent: false,
       },
     ]
   },
